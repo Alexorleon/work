057a33b67e8b9164441452f031e75c6dabc7c3a9
Alexorleon <alexorleon@gmail.com>
2014-09-03 17:00:02 +0700

    СС в один файл и по функциям - реализовано

--- inc/trial_testing.php
+++ inc/trial_testing.php
@@ -5,42 +5,105 @@
 
 		if($_GET['qtype'] == 3){ // пробное тестирование
 		
-
-		}else if($_GET['qtype'] == 4){ // просто тестирование
+			// если еще ни разу не отвечали, то требуются подготовительные действия
+			if ($_SESSION['counter_questions'] == 0){
+			
+				// подготовка
+				preparation($db);
+				
+				// задаем вопрос
+				ask_question($db);
+			}else{
+			
+				// Если количество уже заданных вопросов все еще меньше требуемого количества, задаем новый вопрос
+				if($_SESSION['counter_questions'] < $_SESSION['numquestions']){
+				
+					// задаем вопрос
+					ask_question($db);
+				}else{ // иначе переходим в commentAnswer и выводим результаты теста.
 		
+					$_SESSION['counter_questions'] = 0;
+					die('<script>document.location.href= "'.lhost.'/commentAnswer.php?type_exam=2"</script>');
+				}
+				// TODO: сюда уже не попадаем
+			}
+			
+			$question_text = $_SESSION['question_text'];
+			$array_answers = $_SESSION['array_answers'];
+			
+			$smarty->assign("question", $question_text);//вопрос
+			$smarty->assign("array_answers", $array_answers);//ответы
 
+			$smarty->assign("title", "Пробное тестирование");
+
+		}elseif($_GET['qtype'] == 4){ // просто тестирование
+		
+			// если еще ни разу не отвечали, то требуются подготовительные действия
+			if ($_SESSION['counter_questions'] == 0){
+			
+				// подготовка
+				preparation($db);
+				
+				// задаем вопрос
+				ask_question($db);
+			}else{
+			
+				// Если количество уже заданных вопросов все еще меньше требуемого количества, задаем новый вопрос
+				if($_SESSION['counter_questions'] < $_SESSION['numquestions']){
+				
+					// задаем вопрос
+					ask_question($db);
+				}else{ // иначе переходим в commentAnswer и выводим результаты теста.
+		
+					$_SESSION['counter_questions'] = 0;
+					die('<script>document.location.href= "'.lhost.'/commentAnswer.php?type_exam=2"</script>');
+				}
+				// TODO: сюда уже не попадаем
+			}
+			
+			$question_text = $_SESSION['question_text'];
+			$array_answers = $_SESSION['array_answers'];
+			
+			$smarty->assign("question", $question_text);//вопрос
+			$smarty->assign("array_answers", $array_answers);//ответы
+
+			$smarty->assign("title", "Тестирование");
 		}else{
 			die("У меня не прописано, что делать");
 		}
 	}
 
+	$smarty->assign("error_", $error_);
+	$smarty->assign("typetest", 3);
+	$smarty->display("questions.tpl.html");
+
 	if ($_POST){
 
-		$answer = $_POST['answer'];
+		$answer = $_GET['qtype'];
 
-			// выбираем вариант ответа
-			/*if ($answer == "21"){ // 21 это ID в таблице уровень компетенции. магическое число возможно нужно будет заменить.
-				//echo "good"; // правильно
-
-
-				die('<script>document.location.href= "'.lhost.'/question.php?qtype=3"</script>');
-			}else{ //не правильно
-				//echo "not good";
-				// TODO: либо сразу пишем в историю, либо сохраняем список на потом. лучше сразу - частые но маленькие транзакции.
-				die('<script>document.location.href= "'.lhost.'/question.php?qtype=3"</script>');
-			}*/
-			die('<script>document.location.href= "'.lhost.'/question.php?qtype=3"</script>');
+		if ($answer == 3){ // пробное тестирование
+			
+			//echo "trial testing";
+		}elseif ($answer == 4){ // просто тестирование
+			// пишем в историю
+			// TODO: либо сразу пишем в историю, либо сохраняем список на потом. лучше сразу - частые но маленькие транзакции.
+			//write_history();
+		}else{
+		}
 	}
-
-	// если еще ни разу не отвечали, то требуются подготовительные действия
-	if ($_SESSION['counter_questions'] == 0){
-		
+	
+	// --- ФУНКЦИИ ---
+	
+	// подготовка
+	function preparation(&$obj){
+	
 		$sotrud_dolj = $_SESSION['sotrud_dolj'];
 		
 		/*
 		формируем равномерный массив вопросов по уровню их риска.
 		для этого берем все вопросы по каждому риску. Это будет массив из ID вопросов.
-		*/
+		*/		
+		// TODO: МОДУЛЬ 5 и ТИП ВОПРОСА текстовый
 		
 		// вопросы по смертельному риску
 		$sql_ques = <<<SQL
@@ -48,7 +111,7 @@
 			(SELECT ALLQUESTIONSID FROM stat.ALLQUESTIONS_B WHERE ALLQUESTIONS_B.TESTNAMESID IN 
 			(SELECT TESTNAMESID FROM stat.SPECIALITY_B WHERE SPECIALITY_B.DOLJNOSTKOD='$sotrud_dolj'))
 SQL;
-		$array_death_risk = $db->go_result($sql_ques);
+		$array_death_risk = $obj->go_result($sql_ques);
 		shuffle($array_death_risk);
 		
 		// вопросы по высокому риску
@@ -57,7 +120,7 @@ SQL;
 			(SELECT ALLQUESTIONSID FROM stat.ALLQUESTIONS_B WHERE ALLQUESTIONS_B.TESTNAMESID IN 
 			(SELECT TESTNAMESID FROM stat.SPECIALITY_B WHERE SPECIALITY_B.DOLJNOSTKOD='$sotrud_dolj'))
 SQL;
-		$array_high_risk = $db->go_result($sql_ques);
+		$array_high_risk = $obj->go_result($sql_ques);
 		shuffle($array_high_risk);
 		
 		// вопросы по существенному риску
@@ -66,15 +129,15 @@ SQL;
 			(SELECT ALLQUESTIONSID FROM stat.ALLQUESTIONS_B WHERE ALLQUESTIONS_B.TESTNAMESID IN 
 			(SELECT TESTNAMESID FROM stat.SPECIALITY_B WHERE SPECIALITY_B.DOLJNOSTKOD='$sotrud_dolj'))
 SQL;
-		$array_sign_risk = $db->go_result($sql_ques);
+		$array_sign_risk = $obj->go_result($sql_ques);
 		shuffle($array_sign_risk);
 		
 		// формируем основной массив вопросов необходимого количества
 		$tempcount = $_SESSION['numquestions']; // необходимое количество
 		$final_array = array();
-		//$final_array = array('41','42','43');
+		
 		// берем поочередно из каждого массива ID вопроса
-		$i = 0;
+		$count_i = 0;
 		$b_dr = false;
 		$b_hr = false;
 		$b_sr = false;
@@ -85,13 +148,13 @@ SQL;
 			
 			if($count_ques >= $tempcount) break; // набрали нужное количество вопросов
 			// если массив себя исчерпал, проходим мимо
-			if(count($array_death_risk) <= $i){
+			if(count($array_death_risk) <= $count_i){
 			
 				// берем значение из другого массива
 			}else{
 				// если еще не добавили
 				if(!$b_dr){
-					array_push($final_array, $array_death_risk[$i]);
+					array_push($final_array, $array_death_risk[$count_i]);
 					$b_dr = true;
 					$count_ques++;
 				}
@@ -99,10 +162,10 @@ SQL;
 			
 			if($count_ques >= $tempcount) break;
 			
-			if(count($array_high_risk) <= $i){
+			if(count($array_high_risk) <= $count_i){
 			}else{
 				if(!$b_hr){
-					array_push($final_array, $array_high_risk[$i]);
+					array_push($final_array, $array_high_risk[$count_i]);
 					$b_hr = true;
 					$count_ques++;
 				}
@@ -110,10 +173,10 @@ SQL;
 			
 			if($count_ques >= $tempcount) break;
 			
-			if(count($array_sign_risk) <= $i){
+			if(count($array_sign_risk) <= $count_i){
 			}else{
 				if(!$b_sr){
-					array_push($final_array, $array_sign_risk[$i]);
+					array_push($final_array, $array_sign_risk[$count_i]);
 					$b_sr = true;
 					$count_ques++;
 				}
@@ -122,7 +185,7 @@ SQL;
 			// проверяем что все вложились или больше нечего добавить
 			if($b_dr == true && $b_hr == true && $b_sr == true){ // все гуд, с каждого по вопросу
 			
-				$i++;
+				$count_i++;
 				$b_dr = false;
 				$b_hr = false;
 				$b_sr = false;
@@ -133,7 +196,7 @@ SQL;
 				break;
 			}else{ // ага, кто то не вложидся, берем у других
 				
-				$i++;
+				$count_i++;
 				$b_dr = false;
 				$b_hr = false;
 				$b_sr = false;
@@ -147,15 +210,16 @@ SQL;
 		
 			$_SESSION['final_array'][] = $element;
 		}
-		//print_r($_SESSION['final_array']);
-		//print_r($final_array);
-		//die();
-		// задаем вопрос
-		//ask_question($db);
 		
+		//print_r($_SESSION['final_array']);
+		//die();
+	}
+	
+	// задаем вопрос
+	function ask_question(&$obj){
 		
 		// TODO: тут должен быть вывод вопроса в зависимости от его типа (свое оформление и запрос)
-
+		
 		// стартуем таймер
 		//$_SESSION['DATEBEGIN'] = date('d.m.y H:i:s'); в пробном тесте не нужен
 
@@ -166,19 +230,22 @@ SQL;
 		$sql = <<<SQL
 		SELECT TEXT FROM stat.ALLQUESTIONS WHERE ALLQUESTIONS.ID='$temp_testid'
 SQL;
-		$s_res = $db->go_result_once($sql);
-		$temp_id = (int)$testid['ID'];
+		$s_res = $obj->go_result_once($sql);
+		//$temp_id = (int)$testid['ID'];
 		$question_text = $s_res['TEXT'];
 
 		// берем ответы к этому вопросу
 		$sql_ans = <<<SQL
-		SELECT ID, TEXT, COMPETENCELEVELID FROM stat.ALLANSWERS WHERE ALLANSWERS.ALLQUESTIONSID='$temp_id'
+		SELECT ID, TEXT, COMPETENCELEVELID FROM stat.ALLANSWERS WHERE ALLANSWERS.ALLQUESTIONSID='$temp_testid'
 SQL;
-		$array_answers = $db->go_result($sql_ans);
+		$array_answers = $obj->go_result($sql_ans);
 
 		shuffle($array_answers);
 
 		$_SESSION['counter_questions']++;
+		
+		$_SESSION['question_text'] = $question_text;
+		$_SESSION['array_answers'] = $array_answers;
 		
 		/*print_r($array_death_risk);
 		echo "<br />";
@@ -187,61 +254,10 @@ SQL;
 		print_r($array_sign_risk);
 		echo "<br />";
 		print_r($final_array);*/
-	}else{
-
-		// Если количество уже заданных вопросов все еще меньше требуемого количества, задаем новый вопрос
-		if($_SESSION['counter_questions'] < $_SESSION['numquestions']){
-
-			//ask_question($db);
-				
-			// TODO: тут должен быть вывод вопроса в зависимости от его типа (свое оформление и запрос)
-
-			// стартуем таймер
-			//$_SESSION['DATEBEGIN'] = date('d.m.y H:i:s'); в пробном тесте не нужен
-
-			$testid = $_SESSION['final_array'][$_SESSION['counter_questions']];
-			//print_r("===".$testid);
-			$temp_testid = (int)$testid['ID'];
-
-			$sql = <<<SQL
-			SELECT ID, TEXT FROM stat.ALLQUESTIONS WHERE ALLQUESTIONS.ID='$temp_testid'
-SQL;
-			$s_res = $db->go_result_once($sql);
-
-			$temp_id = (int)$testid['ID'];
-			
-			$question_text = $s_res['TEXT'];
-
-			// берем ответы к этому вопросу
-			$sql_ans = <<<SQL
-			SELECT ID, TEXT, COMPETENCELEVELID FROM stat.ALLANSWERS WHERE ALLANSWERS.ALLQUESTIONSID='$temp_id'
-SQL;
-			$array_answers = $db->go_result($sql_ans);
-
-			shuffle($array_answers);
-
-			$_SESSION['counter_questions']++;
-		
-		}else{ // иначе переходим в commentAnswer и выводим результаты теста.
-		
-			$_SESSION['counter_questions'] = 0;
-			die('<script>document.location.href= "'.lhost.'/commentAnswer.php?type_exam=2"</script>');
-		}
 	}
 	
-	$smarty->assign("error_", $error_);
-	$smarty->assign("question", $question_text);//вопрос
-	$smarty->assign("array_answers", $array_answers);//ответы
+	// пишем в историю
+	function write_history(&$obj){
 
-	$smarty->assign("typetest", 3);
-	$smarty->assign("title", "Контроль компетентности");
-	$smarty->display("questions.tpl.html");
-	
-	// --- ФУНКЦИИ ---
-	
-	// задаем вопрос
-	function ask_question(&$obj){
-		
-		
 	}
 ?>
