ed2dc5e516322793071845bef6a73331a655a509
Alexorleon <alexorleon@gmail.com>
2014-09-10 11:59:01 +0700

    предложение руководству - добавил поле, клавиатуру, тип сообщения

--- proposals.php
+++ proposals.php
@@ -13,12 +13,26 @@
 
 			// переходим в лобби
 			die('<script>document.location.href= "'.lhost.'/index.php"</script>');
-		}
+		}elseif($type_proposals == 1){
+		
+			// отправляем сообщение - добавляем в БД.
+			print_r("send send send");
+			
+			// доложить об успешности или не успешности отправления, но не обновлять страницу!
+			
+		}else{}
 	}
 	
+	// получаем типы сообщения			
+	$sql = <<<SQL
+	SELECT ID, TITLE FROM stat.PROPOSALS_TYPEMES
+SQL;
+	$array_typemes = $db->go_result($sql);
+
 	// TODO: получить историю отправленных сообщений (и показать прочитанные)
 	
 	$smarty->assign("error_", $error_);
+	$smarty->assign("array_typemes", $array_typemes);
 
 	$smarty->assign("title", "Предложения руководству");
 	$smarty->display("proposals.tpl.html");
