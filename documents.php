52207338141b7f9112382eea02d19e9ca0626584
Alexorleon <alexorleon@gmail.com>
2014-09-15 13:06:38 +0700

    теперь одна таблица для обучающих документов

--- documents.php
+++ documents.php
@@ -12,22 +12,11 @@
 		
 			if (isset($_GET['norm_doc_id']) && !empty($_GET['norm_doc_id'])){
 			
-				print_r("Вывести документы - ".$_GET['norm_doc_id']);
-			}else{
-				print_r("<a href='/show_regulations_1'>Нормативный1</a><br>");
-				print_r("<a href='/show_regulations_2'>Нормативный2</a><br>");
-				print_r("<a href='/show_regulations_3'>Нормативный3</a><br>");
-				print_r("<a href='/show_regulations_4'>Нормативный4</a><br>");
-			}
-		}elseif ($_GET['type_doc'] == 2){ // видеоинструктажи
-		
-			if (isset($_GET['video_id']) && !empty($_GET['video_id'])){
-			
-				$video_id = $_GET['video_id'];
+				/*$video_id = $_GET['video_id'];
 				
 				// получаем необходимое видео
 				$sql = <<<SQL
-				SELECT VIDEO FROM stat.INSTRUCTIONALVIDEO WHERE INSTRUCTIONALVIDEO.ID='$video_id'
+				SELECT NAME FROM stat.ALLTRAINING WHERE ALLTRAINING.ID='$video_id'
 SQL;
 				$video_instr = $db->go_result_once($sql);
 				
@@ -38,11 +27,49 @@ SQL;
 				
 				$sotrud_dolj = $_SESSION['sotrud_dolj'];
 				
-				// получаем видеоинструкции				
+				// получаем нормативные документы				
 				$sql = <<<SQL
-				SELECT ID, TITLE FROM stat.INSTRUCTIONALVIDEO WHERE INSTRUCTIONALVIDEO.ID IN
-				(SELECT INSTRUCTIONALVIDEOID FROM stat.INSTVID_B_TN WHERE INSTVID_B_TN.TESTNAMESID IN 
-				(SELECT TESTNAMESID FROM stat.SPECIALITY_B WHERE SPECIALITY_B.DOLJNOSTKOD='$sotrud_dolj'))
+				SELECT ID, TITLE FROM stat.ALLTRAINING WHERE ALLTRAINING.ID IN
+				(SELECT ALLTRAININGID FROM stat.ALLTRAINING_B_TN WHERE ALLTRAINING_B_TN.TESTNAMESID IN 
+				(SELECT TESTNAMESID FROM stat.SPECIALITY_B WHERE SPECIALITY_B.DOLJNOSTKOD='$sotrud_dolj')) 
+				AND ALLTRAINING.ALLTRAININGTYPEID=2
+SQL;
+				$array_instr = $db->go_result($sql);
+				
+				// TODO: сделать 2 запроса,с выборкой из _b. с Y и без.
+				// !!!!! заменить Y на 0 и 1, где 1 это прочитано. Теперь можно просто сортировать при выборке и массив 
+				// заполнится в порядке прочтения. С другой стороны, это медленне чем 2 запроса.
+				
+				//$sotrud_id = $_SESSION['sotrud_id'];
+				
+				$smarty->assign("array_instr", $array_instr);*/
+			}
+		}elseif ($_GET['type_doc'] == 2){ // видеоинструктажи
+		
+			if (isset($_GET['video_id']) && !empty($_GET['video_id'])){
+			
+				$video_id = $_GET['video_id'];
+				
+				// получаем необходимое видео
+				$sql = <<<SQL
+				SELECT NAME FROM stat.ALLTRAINING WHERE ALLTRAINING.ID='$video_id'
+SQL;
+				$video_instr = $db->go_result_once($sql);
+				
+				$smarty->assign("video_instr", $video_instr);
+				//print_r($video_instr['NAME']);
+				//die();
+				
+			}else{
+				
+				$sotrud_dolj = $_SESSION['sotrud_dolj'];
+				
+				// получаем нормативные документы				
+				$sql = <<<SQL
+				SELECT ID, TITLE FROM stat.ALLTRAINING WHERE ALLTRAINING.ID IN
+				(SELECT ALLTRAININGID FROM stat.ALLTRAINING_B_TN WHERE ALLTRAINING_B_TN.TESTNAMESID IN 
+				(SELECT TESTNAMESID FROM stat.SPECIALITY_B WHERE SPECIALITY_B.DOLJNOSTKOD='$sotrud_dolj')) 
+				AND ALLTRAINING.ALLTRAININGTYPEID=2
 SQL;
 				$array_instr = $db->go_result($sql);
 				
@@ -62,7 +89,7 @@ SQL;
 				
 				// получаем необходимое видео
 				$sql = <<<SQL
-				SELECT VIDEO FROM stat.COMPMODACC WHERE COMPMODACC.ID='$comp_model_id'
+				SELECT NAME FROM stat.ALLTRAINING WHERE ALLTRAINING.ID='$comp_model_id'
 SQL;
 				$video_instr = $db->go_result_once($sql);
 				
@@ -73,9 +100,10 @@ SQL;
 				
 				// получаем компьютерные модели				
 				$sql = <<<SQL
-				SELECT ID, TITLE FROM stat.COMPMODACC WHERE COMPMODACC.ID IN
-				(SELECT COMPMODACCID FROM stat.COMPMODACC_B_TN WHERE COMPMODACC_B_TN.TESTNAMESID IN 
-				(SELECT TESTNAMESID FROM stat.SPECIALITY_B WHERE SPECIALITY_B.DOLJNOSTKOD='$sotrud_dolj'))
+				SELECT ID, TITLE FROM stat.ALLTRAINING WHERE ALLTRAINING.ID IN
+				(SELECT ALLTRAININGID FROM stat.ALLTRAINING_B_TN WHERE ALLTRAINING_B_TN.TESTNAMESID IN 
+				(SELECT TESTNAMESID FROM stat.SPECIALITY_B WHERE SPECIALITY_B.DOLJNOSTKOD='$sotrud_dolj')) 
+				AND ALLTRAINING.ALLTRAININGTYPEID=3
 SQL;
 				$array_instr = $db->go_result($sql);
 				
