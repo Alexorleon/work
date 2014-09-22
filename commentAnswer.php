171f002a598cb66cf8e6f89a53ad12f0f889ffa1
Alexorleon <alexorleon@gmail.com>
2014-09-22 16:36:27 +0700

    исправил ошибку. перенес смарти массив только для CC

--- commentAnswer.php
+++ commentAnswer.php
@@ -70,6 +70,8 @@ SQL;
 		//print_r($_SESSION['final_array_answers']);
 		//die();
 		//echo "<br />";
+		$smarty->assign("array_final_qu", $_SESSION['final_array_questions']);
+		$smarty->assign("array_final_an", $_SESSION['final_array_answers']);
 	}else{
 		
 		die("У меня не прописано, что делать");
@@ -103,11 +105,6 @@ SQL;
 	$smarty->assign("question_ans", $question_ans);
 	$smarty->assign("transitionOption", $transitionOption);
 	
-	//$smarty->assign("array_final_qu", $_SESSION['q_final_array']);
-	$smarty->assign("array_final_qu", $_SESSION['final_array_questions']);
-	$smarty->assign("array_final_an", $_SESSION['final_array_answers']);
-	print_r($_SESSION['final_array_questions']);
-
 	$smarty->assign("title", "Комментарий");
 	$smarty->display("commentAnswer.tpl.html");
  ?>
