b384a19a033882dbd75e0bf015e7c5603d7bf56e
Alexorleon <alexorleon@gmail.com>
2014-09-28 13:19:21 +0700

    рабочий вариант

--- inc/ajax.get_sotrud.lamp.php
+++ inc/ajax.get_sotrud.lamp.php
@@ -1,5 +1,7 @@
-<?php	
+<?php
+	//print_r($_POST);
 	//die("Its work mf!!!!!=)s");
+	
 	unset($_SESSION);
 	require_once($_SERVER['DOCUMENT_ROOT']."./cfg/config.inc.php"); 
 	
@@ -11,24 +13,33 @@
 		
 		// получаем табельный и ищем его
 		$check_tab_num = $_POST['check_tab_num'];
-		print_r($check_tab_num);
+		//print_r($check_tab_num);
 	}
 	
 	$period = time() - (3 * 60 * 60); // TODO: установить нужный период
 	$current_date = date('d.m.Y H:i:s', $period);
 		
-	// получаем список сотрудников прошедших предсменный экзаменатор за выбранный период
 	$sql = <<<SQL
 	SELECT TABEL_KADR FROM stat.SOTRUD WHERE SOTRUD.SOTRUD_K IN 
 	(SELECT SOTRUD_ID FROM stat.ALLHISTORY WHERE ALLHISTORY.DATEEND >= to_date('$current_date', 'DD.MM.YYYY HH24:MI:SS') AND 
 	EXAMINERTYPE=1)
 SQL;
-	/*$sql = <<<SQL
-	SELECT SOTRUD.TABEL_KADR, TO_CHAR(ALLHISTORY.DATEEND, 'YY-mm-dd HH24:MI:SS') AS DATEEND FROM stat.SOTRUD, stat.ALLHISTORY WHERE 
-	ALLHISTORY.DATEEND >= to_date('$current_date', 'DD.MM.YYYY HH24:MI:SS') AND SOTRUD.SOTRUD_K IN 
-	(SELECT SOTRUD_ID FROM stat.ALLHISTORY WHERE ALLHISTORY.DATEEND >= to_date('$current_date', 'DD.MM.YYYY HH24:MI:SS') AND 
-	EXAMINERTYPE=1)
-SQL;*/
 	$array_sotrud = $db->go_result($sql);
+
+	// заменяем ID сотрудника на его табельный
+	/*for ($i = 0; $i < count($array_sotrud); $i++){
+	
+		$temp_sotrudid = $array_sotrud[$i]['SOTRUD_ID'];
+				
+		$sql = <<<SQL
+		SELECT TABEL_KADR FROM stat.SOTRUD WHERE SOTRUD.SOTRUD_K='$temp_sotrudid' AND PREDPR_K=10
+SQL;
+		$temp_tabkadr = $db->go_result_once($sql);
+		
+		$array_sotrud[$i]['SOTRUD_ID'] = $temp_tabkadr['TABEL_KADR'];
+	}*/
+	
+	//$array_sotrud = $db->go_result($sql);
 	echo json_encode($array_sotrud);
+	//$smarty->assign("array_sotrud", $array_sotrud);
   ?>
--- lamp.php
+++ lamp.php
@@ -17,18 +17,33 @@
 	// , TO_CHAR(ALLHISTORY.DATEEND, 'YY-mm-dd HH24:MI:SS') AS DATEEND
 	// , stat.ALLHISTORY 
 	// получаем список сотрудников прошедших предсменный экзаменатор за выбранный период
-	/*$sql = <<<SQL
-	SELECT SOTRUD.TABEL_KADR, TO_CHAR(ALLHISTORY.DATEEND, 'YY-mm-dd HH24:MI:SS') AS DATEEND FROM stat.SOTRUD, stat.ALLHISTORY WHERE 
-	ALLHISTORY.DATEEND >= to_date('$current_date', 'DD.MM.YYYY HH24:MI:SS') AND SOTRUD.SOTRUD_K IN 
-	(SELECT SOTRUD_ID FROM stat.ALLHISTORY WHERE ALLHISTORY.DATEEND >= to_date('$current_date', 'DD.MM.YYYY HH24:MI:SS') AND 
-	EXAMINERTYPE=1)
-SQL;*/
-$sql = <<<SQL
+//	select sotrud_id, TO_CHAR(ALLHISTORY.DATEEND, 'YY-mm-dd HH24:MI:SS') AS DATEEND from stat.allhistory where ALLHISTORY.DATEEND >= to_date('$current_date', 'DD.MM.YYYY HH24:MI:SS') AND EXAMINERTYPE=1
+	$sql = <<<SQL
 	SELECT TABEL_KADR FROM stat.SOTRUD WHERE SOTRUD.SOTRUD_K IN 
 	(SELECT SOTRUD_ID FROM stat.ALLHISTORY WHERE ALLHISTORY.DATEEND >= to_date('$current_date', 'DD.MM.YYYY HH24:MI:SS') AND 
 	EXAMINERTYPE=1)
 SQL;
 	$array_sotrud = $db->go_result($sql);
+	
+	// заменяем ID сотрудника на его табельный
+	/*for ($i = 0; $i < count($array_sotrud); $i++){
+	
+		$temp_sotrudid = $array_sotrud[$i]['SOTRUD_ID'];
+				
+		$sql = <<<SQL
+		SELECT TABEL_KADR FROM stat.SOTRUD WHERE SOTRUD.SOTRUD_K='$temp_sotrudid' AND PREDPR_K=10
+SQL;
+		$temp_tabkadr = $db->go_result_once($sql);
+		
+		$array_sotrud[$i]['SOTRUD_ID'] = $temp_tabkadr['TABEL_KADR'];
+	}*/
+	
+	/*$sql = <<<SQL
+	SELECT TABEL_KADR FROM stat.SOTRUD WHERE SOTRUD.SOTRUD_K IN 
+	(SELECT SOTRUD_ID FROM stat.ALLHISTORY WHERE ALLHISTORY.DATEEND >= to_date('$current_date', 'DD.MM.YYYY HH24:MI:SS') AND 
+	EXAMINERTYPE=1)
+SQL;*/
+	//echo "</br>";
 	//print_r($array_sotrud);
 	//die();
 
