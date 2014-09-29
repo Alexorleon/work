40db98901e580f78bc0211bf4fe9d0ceeab63257
Alexorleon <alexorleon@gmail.com>
2014-09-29 14:43:24 +0700

    проверка сдачи контроля

--- inc/ajax.get_sotrud.lamp.php
+++ inc/ajax.get_sotrud.lamp.php
@@ -1,25 +1,12 @@
 <?php
-	//print_r($_POST);
-	//die("Its work mf!!!!!=)s");
-	
 	unset($_SESSION);
 	require_once($_SERVER['DOCUMENT_ROOT']."./cfg/config.inc.php"); 
 	
 	$db = new db;
 	$db->GetConnect();
 	$error_='';
-
-	if ($_POST){
-		
-		// получаем табельный и ищем его
-		$check_tab_num = $_POST['check_tab_num'];
-		
-		/*$sql = <<<SQL
-		SELECT SOTRUD_K FROM stat.SOTRUD WHERE SOTRUD.TABEL_KADR='$check_tab_num'
-SQL;
-		$bool_sotrud = $db->go_result_once($sql);*/
-	}
 	
+if ($_POST['type'] == 1){//тут массив с сотрудниками	
 	$period = time() - (3 * 60 * 60); // TODO: установить нужный период
 	$current_date = date('d.m.Y H:i:s', $period);
 		
@@ -46,4 +33,19 @@ SQL;
 	//$array_sotrud = $db->go_result($sql);
 	echo json_encode($array_sotrud);
 	//echo json_encode($bool_sotrud);
-  ?>
+}else if ($_POST['type'] == 2){//тут проверка табельного
+	// получаем табельный и ищем его
+	$check_tab_num = $_POST['check_tab_num'];
+		
+	$sql = <<<SQL
+	SELECT SOTRUD_K FROM stat.SOTRUD WHERE SOTRUD.TABEL_KADR='$check_tab_num'
+SQL;
+
+	if ($bool_sotrud = $db->go_result_once($sql)){
+		die("good_".$check_tab_num);
+	}else{
+		die("none_".$check_tab_num);
+	}
+	
+}
+?>
