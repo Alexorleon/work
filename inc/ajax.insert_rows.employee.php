c590757090bb394025cdfd21bda1a7d991f8bf3f
Alexorleon <alexorleon@gmail.com>
2014-10-20 16:05:05 +0700

    проверка на совпадение теста к должности

--- inc/ajax.insert_rows.employee.php
+++ inc/ajax.insert_rows.employee.php
@@ -13,11 +13,23 @@
 		$dolj_id = $_POST['dolj_id'];
 		$test_id = $_POST['test_id'];
 		
+		// проверяем на совпадение
 		$sql = <<<SQL
-			insert into stat.speciality_b (TESTNAMESID, DOLJNOSTKOD) VALUES('$test_id', '$dolj_id')
+			SELECT ID FROM stat.SPECIALITY_B WHERE TESTNAMESID='$test_id' AND DOLJNOSTKOD='$dolj_id'
 SQL;
-		$ans = ($db->go_query($sql))?"0":$dolj_id."_".$test_id;
-		die($ans);		
+		$s_res = $db->go_result_once($sql);
+		
+		if(empty($s_res)){
+		
+			$sql = <<<SQL
+				insert into stat.speciality_b (TESTNAMESID, DOLJNOSTKOD) VALUES('$test_id', '$dolj_id')
+SQL;
+			$ans = ($db->go_query($sql))?"0":$dolj_id."_".$test_id;
+			die($ans);
+		}else{
+			$ans = "not";
+			die($ans);
+		}
 	}
 //}
 ?>
