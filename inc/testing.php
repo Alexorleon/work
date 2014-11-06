c21dad9ef5d8b50857e0b18e629d96b58f7f9ad1
ShtirMG <shitrr@gmail.com>
2014-11-06 14:47:45 +0700

    Доработка ЛК

--- inc/testing.php
+++ inc/testing.php
@@ -148,7 +148,7 @@
 SQL;
 		$array_death_risk = $obj->go_result($sql_ques);
 		shuffle($array_death_risk);
-		
+
 		// вопросы по высокому риску
 		$sql_ques = <<<SQL
 			SELECT ID FROM stat.ALLQUESTIONS WHERE ALLQUESTIONS.RISKLEVELID=8 AND ALLQUESTIONS.MODULEID='21' AND ALLQUESTIONS.TYPEQUESTIONSID='10' AND ALLQUESTIONS.ID IN 
@@ -157,7 +157,7 @@ SQL;
 SQL;
 		$array_high_risk = $obj->go_result($sql_ques);
 		shuffle($array_high_risk);
-		
+
 		// вопросы по существенному риску
 		$sql_ques = <<<SQL
 			SELECT ID FROM stat.ALLQUESTIONS WHERE ALLQUESTIONS.RISKLEVELID=9 AND ALLQUESTIONS.MODULEID='21' AND ALLQUESTIONS.TYPEQUESTIONSID='10' AND ALLQUESTIONS.ID IN 
@@ -253,7 +253,7 @@ SQL;
 			$_SESSION['q_final_array'][] = $element;
 		}
 		
-		//print_r($_SESSION['q_final_array']);
+		
 		//die();
 	}
 	
