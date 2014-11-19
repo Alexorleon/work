b61db1bd6b87a921ccad10b1e1148394c6dfdd81
Alexorleon <alexorleon@gmail.com>
2014-11-19 10:33:34 +0700

    разделение редактирования по типам вопросов

---
+++ inc/ajax.get_questions.php
@@ -0,0 +1,103 @@
+<?php
+
+	require_once($_SERVER['DOCUMENT_ROOT']."./cfg/config.inc.php");	
+	$db = new db;
+	$db->GetConnect();
+	$error_='';
+	
+	$type_question = filter_input(INPUT_POST, 'type_question', FILTER_SANITIZE_NUMBER_INT);
+	
+	// TODO: магические числа
+	// получаем необходимые данные по типу вопроса
+	switch ($type_question) {
+		case 8:
+			
+			break;
+		case 9:
+			
+			break;
+		case 10:
+			
+			break;
+		case 21:
+			
+			break;
+		case 22:
+			
+			break;
+	}
+
+if ($temp_type == 1){//тут массив с сотрудниками	
+	$period = time() - (3 * 60 * 60); // TODO: установить нужный период
+	$current_date = date('d.m.Y H:i:s', $period);
+		
+	$sql = <<<SQL
+	SELECT TABEL_KADR FROM stat.SOTRUD WHERE SOTRUD.SOTRUD_K IN 
+	(SELECT SOTRUD_ID FROM stat.ALLHISTORY WHERE ALLHISTORY.DATEBEGIN >= to_date('$current_date', 'DD.MM.YYYY HH24:MI:SS') AND 
+	EXAMINERTYPE=1) ORDER BY TABEL_KADR
+SQL;
+	$array_sotrud = $db->go_result($sql);
+
+	// заменяем ID сотрудника на его табельный
+	/*for ($i = 0; $i < count($array_sotrud); $i++){
+	
+		$temp_sotrudid = $array_sotrud[$i]['SOTRUD_ID'];
+				
+		$sql = <<<SQL
+		SELECT TABEL_KADR FROM stat.SOTRUD WHERE SOTRUD.SOTRUD_K='$temp_sotrudid' AND PREDPR_K=$predpr_k_glob
+SQL;
+		$temp_tabkadr = $db->go_result_once($sql);
+		
+		$array_sotrud[$i]['SOTRUD_ID'] = $temp_tabkadr['TABEL_KADR'];
+	}*/
+	
+	//$array_sotrud = $db->go_result($sql);
+	echo json_encode($array_sotrud);
+	//echo json_encode($bool_sotrud);
+}else if ($temp_type == 2){//тут проверка табельного
+	// получаем табельный и ищем его
+	$check_tab_num = filter_input(INPUT_POST, 'check_tab_num', FILTER_SANITIZE_NUMBER_INT);//$_POST['check_tab_num'];
+		
+	$sql = <<<SQL
+	SELECT SOTRUD_K FROM stat.SOTRUD WHERE SOTRUD.TABEL_KADR='$check_tab_num' AND PREDPR_K=$predpr_k_glob
+SQL;
+	$bool_sotrud = $db->go_result_once($sql);
+
+	if ($bool_sotrud){
+	
+		$temp_sotrud = $bool_sotrud['SOTRUD_K'];
+		
+		$sql = <<<SQL
+		SELECT to_char(MAX(DATEBEGIN), 'DD.MM.YYYY HH24:MI:SS') AS DATEBEGIN FROM stat.ALLHISTORY WHERE ALLHISTORY.SOTRUD_ID='$temp_sotrud' AND EXAMINERTYPE=1
+SQL;
+		$datemax = $db->go_result_once($sql);
+		
+		//print_r($datemax['DATEEND']);
+		//die();
+		
+		die("good_".$check_tab_num."_".$datemax['DATEBEGIN']);
+		//die("good_".$check_tab_num."_".$datemax);
+	}else{
+		die("none_".$check_tab_num);
+	}
+	
+}else if ($temp_type == 3){ // тут надо получить данные по сотруднику
+
+	$sql = <<<SQL
+	SELECT SOTRUD_K FROM stat.SOTRUD WHERE SOTRUD.TABEL_KADR='$sotrud_tabel_kadr' AND PREDPR_K=$predpr_k_glob
+SQL;
+	$sotrud = $db->go_result_once($sql);
+	
+	$temp_sotrud = $sotrud['SOTRUD_K'];
+	
+	$sql = <<<SQL
+		SELECT to_char(MAX(DATEBEGIN), 'DD.MM.YYYY HH24:MI:SS') AS DATEBEGIN FROM stat.ALLHISTORY WHERE ALLHISTORY.SOTRUD_ID='$temp_sotrud' AND EXAMINERTYPE=1
+SQL;
+		$datemax = $db->go_result_once($sql);
+
+		$date = $datemax['DATEBEGIN'];
+		die("date_".$date);
+}else{
+}
+//}
+?>
