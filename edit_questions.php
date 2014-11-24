cbd8fba26c3e9e562e92572dbf0f08730416d778
ShtirMG <shitrr@gmail.com>
2014-11-24 16:52:20 +0700

    Апдейт редактирования и добавления - теперь с ответами

--- edit_questions.php
+++ edit_questions.php
@@ -13,52 +13,6 @@
 	$error_='';
 	$question_id = filter_input(INPUT_GET, 'question_id', FILTER_SANITIZE_NUMBER_INT); //ID вопроса
         
-        $question_data = array();
-        if ($question_id)
-        {
-            $sql = "SELECT
-                    ALLQUESTIONS.TYPEQUESTIONSID AS TYPE, ALLQUESTIONS.MODULEID AS MID, ALLQUESTIONS.RISKLEVELID AS RISK, ALLQUESTIONS.TEXT AS TEXT,
-                    ALLQUESTIONS.SIMPLEPHOTO AS PHOTO, ALLQUESTIONS.SIMPLEVIDEO AS VIDEO,
-                    ALLQUESTIONS_B.TESTNAMESID AS TEST
-                    FROM ALLQUESTIONS, ALLQUESTIONS_B
-                    WHERE ALLQUESTIONS.ID=$question_id AND ALLQUESTIONS_B.ALLQUESTIONSID=ALLQUESTIONS.ID";
-            $q_res = $db->go_result_once($sql);
-            $sql = "SELECT ID, TEXT, PRICE FROM stat.ALLANSWERS WHERE ALLQUESTIONSID='$question_id'";
-            $a_res = $db->go_result($sql);
-            $question_data['id'] = $question_id;
-            $question_data['type'] = $q_res['TYPE'];
-            $question_data['module'] = $q_res['MID'];
-            $question_data['risk'] = $q_res['RISK'];
-            $question_data['test'] = $q_res['TEST'];
-            $question_data['text'] = $q_res['TEXT'];
-            $question_data['photo'] = $q_res['PHOTO'];
-            $question_data['video'] = $q_res['VIDEO'];
-            $question_data['answers'] = $a_res;   
-        }
-        else
-        {
-            $question_data['id'] = 0;
-            $question_data['type'] = 0;
-            $question_data['module'] = 0;
-            $question_data['risk'] = 0;
-            $question_data['test'] = 0;
-            $question_data['text'] = '';
-            $question_data['photo'] = '';
-            $question_data['video'] = '';
-            $question_data['answers'] = array();
-            $question_data['answers'][0] = array();
-            $question_data['answers'][0]['ID'] = '';
-            $question_data['answers'][0]['TEXT'] = '';
-            $question_data['answers'][0]['PRICE'] = '';
-            $question_data['answers'][1] = array();
-            $question_data['answers'][1]['ID'] = '';
-            $question_data['answers'][1]['TEXT'] = '';
-            $question_data['answers'][1]['PRICE'] = '';
-            $question_data['answers'][2] = array();
-            $question_data['answers'][2]['ID'] = '';
-            $question_data['answers'][2]['TEXT'] = '';
-            $question_data['answers'][2]['PRICE'] = '';
-        }
         
         if (!empty($_POST))
         {
@@ -66,7 +20,7 @@
             $type_question = filter_input(INPUT_POST, 'type_question', FILTER_SANITIZE_NUMBER_INT);
             $module_question = filter_input(INPUT_POST,'module_question', FILTER_SANITIZE_NUMBER_INT);
             $risklevel_question = filter_input(INPUT_POST,'risklevel_question', FILTER_SANITIZE_NUMBER_INT);
-            $testname_question = filter_input(INPUT_POST,'type_question', FILTER_SANITIZE_NUMBER_INT);
+            $testname_question = filter_input(INPUT_POST,'testname_question', FILTER_SANITIZE_NUMBER_INT);
             
             $text_question = filter_input(INPUT_POST,'text_question', FILTER_SANITIZE_STRING);
             $text_question = iconv(mb_detect_encoding($text_question), "windows-1251", $text_question);
@@ -74,7 +28,8 @@
             $id_answer = filter_input(INPUT_POST, 'id_answer', FILTER_SANITIZE_NUMBER_INT, FILTER_REQUIRE_ARRAY);
             $text_answer = filter_input(INPUT_POST, 'text_answer', FILTER_SANITIZE_STRING, FILTER_REQUIRE_ARRAY);
             $answer_price = filter_input(INPUT_POST, 'answer_price', FILTER_SANITIZE_NUMBER_INT, FILTER_REQUIRE_ARRAY);
-            
+            $answer_comment = filter_input(INPUT_POST, 'answer_comment', FILTER_SANITIZE_STRING);
+            $answer_factor = filter_input(INPUT_POST, 'answer_factor', FILTER_SANITIZE_STRING);
             if ($current_id)
             {
                 $sql_question = "UPDATE stat.ALLQUESTIONS SET
@@ -100,126 +55,129 @@
             {
                 $competencelevel_id = GetCompetenceLevelID($db, $answer_price);
                 $text_answer[$ans_iter] = iconv(mb_detect_encoding($text_answer[$ans_iter]), "windows-1251", $text_answer[$ans_iter]);
+                if ($answer_price[$ans_iter]!=0)
+                {
+                    $current_comment = iconv(mb_detect_encoding($answer_comment), "windows-1251", $answer_comment);
+                    $current_factor = iconv(mb_detect_encoding($answer_factor), "windows-1251", $answer_factor);
+                    $current_risk = $risklevel_question;
+                }
+                else
+                {
+                    $current_comment = "";
+                    $current_factor = "";
+                    $current_risk = 21;
+                }
                 if ($id_answer[$ans_iter]!=0)
                 {
                     $sql_answer = "UPDATE stat.ALLANSWERS SET
                         TEXT ='{$text_answer[$ans_iter]}',
                         ALLQUESTIONSID='$current_id',
-                        COMPETENCELEVEL";
+                        COMPETENCELEVELID='$competencelevel_id',
+                        COMMENTARY='$current_comment',
+                        FACTOR='$current_factor',
+                        RISKLEVELID='$current_risk',
+                        PRICE='{$answer_price[$ans_iter]}'
+                        WHERE ID='{$id_answer[$ans_iter]}'";
+                }
+                else
+                {
+                    $sql_answer = "INSERT INTO stat.ALLANSWERS
+                        (TEXT, ALLQUESTIONSID, COMPETENCELEVELID, COMMENTARY, FACTOR, RISKLEVELID, PRICE)
+                        VALUES
+                        ('{$text_answer[$ans_iter]}','$current_id','$competencelevel_id','$current_comment','$current_factor','$current_risk','{$answer_price[$ans_iter]}')";
+                }
+                $db->go_query($sql_answer);
+            }
+            
+            $sql_AQB = "SELECT ID FROM stat.ALLQUESTIONS_B WHERE ALLQUESTIONSID='$current_id'";
+            $test_id = $db->go_result_once($sql_AQB)['ID'];
+            if ($test_id)
+            {
+                $sql_AQB = "UPDATE stat.ALLQUESTIONS_B SET TESTNAMESID='$testname_question' WHERE ID='$test_id'";
+            }
+            else
+            {
+                $sql_AQB = "INSERT INTO stat.ALLQUESTIONS_B (TESTNAMESID, ALLQUESTIONSID) VALUES ('$testname_question', '$current_id')";
+            }
+            $db->go_query($sql_AQB);
+        }
+        
+        $question_data = array();
+        if ($question_id)
+        {
+            $sql = "SELECT
+                    ALLQUESTIONS.TYPEQUESTIONSID AS TYPE, ALLQUESTIONS.MODULEID AS MID, ALLQUESTIONS.RISKLEVELID AS RISK, ALLQUESTIONS.TEXT AS TEXT,
+                    ALLQUESTIONS.SIMPLEPHOTO AS PHOTO, ALLQUESTIONS.SIMPLEVIDEO AS VIDEO,
+                    ALLQUESTIONS_B.TESTNAMESID AS TEST
+                    FROM ALLQUESTIONS, ALLQUESTIONS_B
+                    WHERE ALLQUESTIONS.ID=$question_id AND ALLQUESTIONS_B.ALLQUESTIONSID=ALLQUESTIONS.ID";
+            $q_res = $db->go_result_once($sql);
+            $sql = "SELECT ID, TEXT, PRICE, COMMENTARY, FACTOR FROM stat.ALLANSWERS WHERE ALLQUESTIONSID='$question_id'";
+            $a_res = $db->go_result($sql);
+            foreach($a_res as $answer)
+            {
+                if ($answer['PRICE']!=0)
+                {
+                    $question_data['commentary'] = $answer['COMMENTARY'];
+                    $question_data['factor'] = $answer['FACTOR'];
+                    break;
                 }
             }
+            $question_data['id'] = $question_id;
+            $question_data['type'] = $q_res['TYPE'];
+            $question_data['module'] = $q_res['MID'];
+            $question_data['risk'] = $q_res['RISK'];
+            $question_data['test'] = $q_res['TEST'];
+            $question_data['text'] = $q_res['TEXT'];
+            $question_data['photo'] = $q_res['PHOTO'];
+            $question_data['video'] = $q_res['VIDEO'];
+          //  $question_data['commentary'] = $a_res[0]['COMMENTARY'];
+          //  $question_data['factor'] = $a_res[0]['FACTOR'];
+            $question_data['answers'] = $a_res;
+            }
+        else
+        {
+            $question_data['id'] = 0;
+            $question_data['type'] = 0;
+            $question_data['module'] = 0;
+            $question_data['risk'] = 0;
+            $question_data['test'] = 0;
+            $question_data['text'] = '';
+            $question_data['photo'] = '';
+            $question_data['video'] = '';
+            $question_data['answers'] = array();
+            $question_data['answers'][0] = array();
+            $question_data['answers'][0]['ID'] = '';
+            $question_data['answers'][0]['TEXT'] = '';
+            $question_data['answers'][0]['PRICE'] = '';
+            $question_data['answers'][1] = array();
+            $question_data['answers'][1]['ID'] = '';
+            $question_data['answers'][1]['TEXT'] = '';
+            $question_data['answers'][1]['PRICE'] = '';
+            $question_data['answers'][2] = array();
+            $question_data['answers'][2]['ID'] = '';
+            $question_data['answers'][2]['TEXT'] = '';
+            $question_data['answers'][2]['PRICE'] = '';
         }
-	/*if (!empty($_POST)){
-		$employeesur = filter_input(INPUT_POST, 'employeesur', FILTER_SANITIZE_SPECIAL_CHARS);
-		$employeesur = iconv("utf-8", "windows-1251", $employeesur); // фамилия
-                
-        $employeename = filter_input(INPUT_POST, 'employeename', FILTER_SANITIZE_SPECIAL_CHARS);
-		$employeename = iconv("utf-8", "windows-1251", $employeename); // имя
-                
-        $employeepat = filter_input(INPUT_POST, 'employeepat', FILTER_SANITIZE_SPECIAL_CHARS);
-		$employeepat = iconv("utf-8", "windows-1251", $employeepat); // отчество
-                
-		$type_doljnost = filter_input(INPUT_POST, 'type_doljnost', FILTER_SANITIZE_SPECIAL_CHARS);//$_POST['type_doljnost']; // должность
-		
-        $employeetabel = filter_input(INPUT_POST, 'employeetabel', FILTER_SANITIZE_NUMBER_INT);//$_POST['employeetabel']; // табельный
-		
-		// определяем нужный запрос в зависимости от статуса. добавляем или редактируем
-		if($_SESSION['add_or_edit_questions'] == 0){ // это добавление нового
-		
-			// проверяем табельный номер
-			$sql = <<<SQL
-			SELECT SOTRUD_K FROM stat.SOTRUD WHERE PREDPR_K='$predpr_k_glob' AND SOTRUD.TABEL_KADR='$employeetabel'
-SQL;
-			$check_employees_tabel = $db->go_result_once($sql);
+        
 
-			if(empty($check_employees_tabel)){ // если пусто, то такого табельного нет. добовляем.
-				$error_='';//нулим ошибку,если повторно будет
-				$smarty->assign("employeename", "");
-				$sql = <<<SQL
-				INSERT INTO stat.SOTRUD (SOTRUD_FAM, SOTRUD_IM, SOTRUD_OTCH, PREDPR_K, DOLJ_K, TABEL_KADR) 
-				VALUES ('$employeesur', '$employeename', '$employeepat', '$predpr_k_glob', '$type_doljnost', '$employeetabel')
-SQL;
-				$db->go_query($sql);
-			
-			}else{ // иначе говорим что такой табельный уже есть
-				
-				//Во первых, нужно вывести ошибку, точнее текст ошибки
-				$error_ = "Такой табельный уже есть!";
-			}
-		
-		}else if($_SESSION['add_or_edit_questions'] == 1){ // это редактирование
-	
-			$employee_id_hidden = filter_input(INPUT_POST, 'employee_hidden_id', FILTER_SANITIZE_NUMBER_INT);//$_POST['employee_hidden_id'];
-			//print_r($_POST);
-			//die();
-			
-			// проверяем табельный номер. но если это тот же, то все норм.
-			if($_SESSION['check_employee_tabel'] != $employeetabel){
-
-				$sql = <<<SQL
-				SELECT SOTRUD_K FROM stat.SOTRUD WHERE PREDPR_K='$predpr_k_glob' AND SOTRUD.TABEL_KADR='$employeetabel'
-SQL;
-				$check_employees_tabel = $db->go_result_once($sql);
-
-				if(empty($check_employees_tabel)){ // если пусто, то такого табельного нет. добовляем.
-
-					$sql = <<<SQL
-					UPDATE stat.SOTRUD SET SOTRUD_FAM='$employeesur', SOTRUD_IM='$employeename', SOTRUD_OTCH='$employeepat', DOLJ_K='$type_doljnost', TABEL_KADR='$employeetabel' WHERE 
-					SOTRUD.PREDPR_K='$predpr_k_glob' AND SOTRUD.SOTRUD_K='$employee_id_hidden'
-SQL;
-					$db->go_query($sql);
-					
-					// обновляем данные в полях
-					$_GET['employee_cur'] = $employeesur; // фамилия
-					$_GET['employee_name'] = $employeename; // имя
-					$_GET['employee_pat'] = $employeepat; // отчество
-					$_GET['employee_tabel'] = $employeetabel; // табельный
-					$_GET['dolj'] = $type_doljnost; // ID должности
-					
-					// запоминаем новый табельный
-					$_SESSION['check_employee_tabel'] = $employeetabel;
-				}else{ // иначе говорим что такой табельный уже есть
-					
-					//Во первых, нужно вывести ошибку, точнее текст ошибки
-					$error_ = "Такой табельный уже есть!";
-				}
-			}else{
-			
-				$sql = <<<SQL
-					UPDATE stat.SOTRUD SET SOTRUD_FAM='$employeesur', SOTRUD_IM='$employeename', SOTRUD_OTCH='$employeepat', DOLJ_K='$type_doljnost', TABEL_KADR='$employeetabel' WHERE 
-					SOTRUD.PREDPR_K='$predpr_k_glob' AND SOTRUD.SOTRUD_K='$employee_id_hidden'
-SQL;
-				$db->go_query($sql);
-				
-				// обновляем данные в полях
-				$_GET['employee_cur'] = $employeesur; // фамилия
-				$_GET['employee_name'] = $employeename; // имя
-				$_GET['employee_pat'] = $employeepat; // отчество
-				$_GET['employee_tabel'] = $employeetabel; // табельный
-				$_GET['dolj'] = $type_doljnost; // ID должности
-			}
-		}else{
-			
-			die("У меня не прописано, что делать");
-		}
-	}*/
 	$role = filter_input(INPUT_COOKIE, 'role', FILTER_SANITIZE_NUMBER_INT);
-    
+
 	$smarty->assign("role", $role);
 	if(array_key_exists('posttype', $_GET)){
-	
+
 		$posttype = filter_input(INPUT_GET, 'posttype', FILTER_SANITIZE_NUMBER_INT);
 		if($posttype == 0){ // это добавление нового
-		
+
 			$_SESSION['add_or_edit_questions'] = 0;
-			
+
 			// чистые значения
 			$smarty->assign("text_question", '');
-			
+
 		}else if($posttype == 1){ // это редактирование
-	
+
 			$_SESSION['add_or_edit_questions'] = 1;
-			
+
 			// получаем значения для задания их по умолчанию
 			//$employee_id = filter_input(INPUT_GET, 'employee_id', FILTER_SANITIZE_NUMBER_INT); //$_GET['employee_id']; // id сотрудника
 
