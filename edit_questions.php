f092ac6ca56f31b88f382d35258e96965e6a664f
ShtirMG <shitrr@gmail.com>
2014-11-24 15:19:44 +0700

    Редактирование и добавление вопросов

--- edit_questions.php
+++ edit_questions.php
@@ -11,7 +11,104 @@
 	$db = new db;
 	$db->GetConnect();
 	$error_='';
-	
+	$question_id = filter_input(INPUT_GET, 'question_id', FILTER_SANITIZE_NUMBER_INT); //ID вопроса
+        
+        $question_data = array();
+        if ($question_id)
+        {
+            $sql = "SELECT
+                    ALLQUESTIONS.TYPEQUESTIONSID AS TYPE, ALLQUESTIONS.MODULEID AS MID, ALLQUESTIONS.RISKLEVELID AS RISK, ALLQUESTIONS.TEXT AS TEXT,
+                    ALLQUESTIONS.SIMPLEPHOTO AS PHOTO, ALLQUESTIONS.SIMPLEVIDEO AS VIDEO,
+                    ALLQUESTIONS_B.TESTNAMESID AS TEST
+                    FROM ALLQUESTIONS, ALLQUESTIONS_B
+                    WHERE ALLQUESTIONS.ID=$question_id AND ALLQUESTIONS_B.ALLQUESTIONSID=ALLQUESTIONS.ID";
+            $q_res = $db->go_result_once($sql);
+            $sql = "SELECT ID, TEXT, PRICE FROM stat.ALLANSWERS WHERE ALLQUESTIONSID='$question_id'";
+            $a_res = $db->go_result($sql);
+            $question_data['id'] = $question_id;
+            $question_data['type'] = $q_res['TYPE'];
+            $question_data['module'] = $q_res['MID'];
+            $question_data['risk'] = $q_res['RISK'];
+            $question_data['test'] = $q_res['TEST'];
+            $question_data['text'] = $q_res['TEXT'];
+            $question_data['photo'] = $q_res['PHOTO'];
+            $question_data['video'] = $q_res['VIDEO'];
+            $question_data['answers'] = $a_res;   
+        }
+        else
+        {
+            $question_data['id'] = 0;
+            $question_data['type'] = 0;
+            $question_data['module'] = 0;
+            $question_data['risk'] = 0;
+            $question_data['test'] = 0;
+            $question_data['text'] = '';
+            $question_data['photo'] = '';
+            $question_data['video'] = '';
+            $question_data['answers'] = array();
+            $question_data['answers'][0] = array();
+            $question_data['answers'][0]['ID'] = '';
+            $question_data['answers'][0]['TEXT'] = '';
+            $question_data['answers'][0]['PRICE'] = '';
+            $question_data['answers'][1] = array();
+            $question_data['answers'][1]['ID'] = '';
+            $question_data['answers'][1]['TEXT'] = '';
+            $question_data['answers'][1]['PRICE'] = '';
+            $question_data['answers'][2] = array();
+            $question_data['answers'][2]['ID'] = '';
+            $question_data['answers'][2]['TEXT'] = '';
+            $question_data['answers'][2]['PRICE'] = '';
+        }
+        
+        if (!empty($_POST))
+        {
+            $current_id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+            $type_question = filter_input(INPUT_POST, 'type_question', FILTER_SANITIZE_NUMBER_INT);
+            $module_question = filter_input(INPUT_POST,'module_question', FILTER_SANITIZE_NUMBER_INT);
+            $risklevel_question = filter_input(INPUT_POST,'risklevel_question', FILTER_SANITIZE_NUMBER_INT);
+            $testname_question = filter_input(INPUT_POST,'type_question', FILTER_SANITIZE_NUMBER_INT);
+            
+            $text_question = filter_input(INPUT_POST,'text_question', FILTER_SANITIZE_STRING);
+            $text_question = iconv(mb_detect_encoding($text_question), "windows-1251", $text_question);
+            
+            $id_answer = filter_input(INPUT_POST, 'id_answer', FILTER_SANITIZE_NUMBER_INT, FILTER_REQUIRE_ARRAY);
+            $text_answer = filter_input(INPUT_POST, 'text_answer', FILTER_SANITIZE_STRING, FILTER_REQUIRE_ARRAY);
+            $answer_price = filter_input(INPUT_POST, 'answer_price', FILTER_SANITIZE_NUMBER_INT, FILTER_REQUIRE_ARRAY);
+            
+            if ($current_id)
+            {
+                $sql_question = "UPDATE stat.ALLQUESTIONS SET
+                        TEXT='$text_question',
+                        TYPEQUESTIONSID='$type_question',
+                        MODULEID='$module_question',
+                        RISKLEVELID='$risklevel_question'
+                        WHERE ID='$current_id'";
+                $db->go_query($sql_question);
+            }
+            else
+            {
+                $sql_question = "INSERT INTO stat.ALLQUESTIONS
+                                (TEXT, TYPEQUESTIONSID, MODULEID, RISKLEVELID)
+                                VALUES ('$text_question', '$type_question', '$module_question', '$risklevel_question')
+                                  returning ID into :mylastid";
+                $stmt = OCIParse($c, $sql_question);
+                oci_bind_by_name($stmt, "mylastid", $current_id, 32, SQLT_INT);
+                OCIExecute($stmt);
+            }
+            
+            for ($ans_iter = 0; $ans_iter<3; $ans_iter++)
+            {
+                $competencelevel_id = GetCompetenceLevelID($db, $answer_price);
+                $text_answer[$ans_iter] = iconv(mb_detect_encoding($text_answer[$ans_iter]), "windows-1251", $text_answer[$ans_iter]);
+                if ($id_answer[$ans_iter]!=0)
+                {
+                    $sql_answer = "UPDATE stat.ALLANSWERS SET
+                        TEXT ='{$text_answer[$ans_iter]}',
+                        ALLQUESTIONSID='$current_id',
+                        COMPETENCELEVEL";
+                }
+            }
+        }
 	/*if (!empty($_POST)){
 		$employeesur = filter_input(INPUT_POST, 'employeesur', FILTER_SANITIZE_SPECIAL_CHARS);
 		$employeesur = iconv("utf-8", "windows-1251", $employeesur); // фамилия
@@ -149,6 +246,7 @@ SQL;
         
 	$smarty->assign("error_", $error_);
 	
+        $smarty->assign("question_data", $question_data);
 	$smarty->assign("array_typequestions", $array_typequestions);
 	$smarty->assign("array_module", $array_module);
 	$smarty->assign("array_risklevel", $array_risklevel);
@@ -158,5 +256,20 @@ SQL;
 	$smarty->display("edit_questions.tpl.html");
 
 	// --- ФУНКЦИИ ---
-
+function GetCompetenceLevelID($obj, $level_num)
+{
+    $sql_competencelevel = <<<SQL
+                   SELECT ID, PENALTYPOINTS_MIN FROM stat.COMPETENCELEVEL ORDER BY PENALTYPOINTS_MAX
+SQL;
+    $competencelevel = $obj->go_result($sql_competencelevel);
+    $result = '';
+    foreach ($competencelevel as $cl)
+    {
+        if ($cl['PENALTYPOINTS_MIN']<=$level_num)
+        {
+           $result = $cl['ID']; 
+        }
+    }
+    return  $result;
+}
   ?>
--- inc/ajax.get_questions.php
+++ inc/ajax.get_questions.php
@@ -1,34 +1,42 @@
 <?php
 
+//error_reporting(E_ALL);
 	require_once($_SERVER['DOCUMENT_ROOT']."./cfg/config.inc.php");	
 	$db = new db;
 	$db->GetConnect();
 	$error_='';
-	
-	$type_question = filter_input(INPUT_POST, 'typequestion', FILTER_SANITIZE_NUMBER_INT);
-	
+       
+	$type_question = filter_input(INPUT_POST, 'type_question', FILTER_SANITIZE_NUMBER_INT);
+        $download_sv = filter_input(INPUT_POST, 'download_sv', FILTER_SANITIZE_STRING);
+        $download_sf = filter_input(INPUT_POST, 'download_sf', FILTER_SANITIZE_STRING);
+        $text_answer = filter_input(INPUT_POST, 'text_answer', FILTER_SANITIZE_STRING, FILTER_REQUIRE_ARRAY);
+        $answer_price = filter_input(INPUT_POST, 'answer_price', FILTER_SANITIZE_NUMBER_INT, FILTER_REQUIRE_ARRAY);
+        
 	// TODO: магические числа
 	// получаем необходимые данные по типу вопроса
 	switch ($type_question) {
                 case 8: //текстовый вопрос
 		?>
+                <input type='hidden' name='download_sv' value='<?=$download_sv?>'/>
+                <input type='hidden' name='download_sf' value='<?=$download_sf?>'/>
                 <table>
                     <tr>
 			<td>Ответ 1:</td>
                         <td>
-                            <textarea min-length=2 aria-required="true" name="text_answer[1]" maxlength="150" rows="2" cols="75" required style="resize: none;"  data-validation="length" data-validation-length="max150" data-validation-error-msg="Допустимая длина не более 150 символов!"></textarea>
+                            <textarea min-length=2 aria-required="true" name="text_answer[0]" maxlength="150" rows="2" cols="75" required style="resize: none;"  data-validation="length" data-validation-length="max150" data-validation-error-msg="Допустимая длина не более 150 символов!"><?=$text_answer[0]?></textarea>
                         </td>
                         
                     </tr>
                     <tr>
                         <td>Штраф за ответ:</td>
                         <td>
-                            <input  class="admin_input_text"  type="text" size="2" name="answer_price[1]" data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"/>
+                            <input  class="admin_input_text"  type="text" size="2" name="answer_price[0]" value="<?=$answer_price[0]?>" data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"/>
                         </td>
                     </tr>
                     <tr>
 			<td>Ответ 2:</td>
-                        <td><textarea name="text_answer[2]" maxlength="150" rows="2" cols="75" required style="resize: none;" data-validation="length" data-validation-length="max150" data-validation-error-msg="Допустимая длина не более 150 символов!"></textarea>
+                        <td>
+                            <textarea name="text_answer[1]" maxlength="150" rows="2" cols="75" required style="resize: none;" data-validation="length" data-validation-length="max150" data-validation-error-msg="Допустимая длина не более 150 символов!"><?=$text_answer[1]?></textarea>
                             <!--<input class="field_text_edit" id="text_question" name="text_question" value="{$text_question}">-->
                         </td>
                         
@@ -36,7 +44,21 @@
                     <tr>
                         <td>Штраф за ответ:</td>
                         <td>
-                            <input class="admin_input_text" type="text" size="2" name="answer_price[2]" data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"/>
+                            <input class="admin_input_text" type="text" size="2" name="answer_price[1]" value="<?=$answer_price[1]?>" data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"/>
+                        </td>
+                    </tr>
+                    <tr>
+			<td>Ответ 3:</td>
+                        <td>
+                            <textarea name="text_answer[2]" maxlength="150" rows="2" cols="75" required style="resize: none;" data-validation="length" data-validation-length="max150" data-validation-error-msg="Допустимая длина не более 150 символов!"><?=$text_answer[2]?></textarea>
+                            <!--<input class="field_text_edit" id="text_question" name="text_question" value="{$text_question}">-->
+                        </td>
+                        
+                    </tr>
+                    <tr>
+                        <td>Штраф за ответ:</td>
+                        <td>
+                            <input class="admin_input_text" type="text" size="2" name="answer_price[2]" value="<?=$answer_price[2]?>" data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"/>
                         </td>
                     </tr>
                 </table>
@@ -44,32 +66,40 @@
 			break;
                 case 9: //Видео-вопрос
                 ?>
-                    
+                    <input type='hidden' name='download_sf' value='<?=$download_sf?>'/>
                     <table>
                         <tr>
                             <td>
                                 Видео:
                             </td>
                             <td>
+                            <?php
+                            if ($download_sv)
+                            {
+                            ?>
+                                Текущий видеофайл: <?=$download_sv?>
+                            <?php
+                            }
+                            ?>
                                 <input type=file id=download_sv name=download_sv accept=video/mp4>
                             </td>
                         </tr>
                     <tr>
 			<td>Ответ 1:</td>
                         <td>
-                            <textarea name="text_answer[1]" maxlength="150" rows="2" cols="75" required style="resize: none;"  data-validation="length" data-validation-length="max150" data-validation-error-msg="Допустимая длина не более 150 символов!"></textarea>
+                            <textarea name="text_answer[0]" maxlength="150" rows="2" cols="75" required style="resize: none;"  data-validation="length" data-validation-length="max150" data-validation-error-msg="Допустимая длина не более 150 символов!"><?=$text_answer[0]?></textarea>
                         </td>
                         
                     </tr>
                     <tr>
                         <td>Штраф за ответ:</td>
                         <td>
-                            <input  class="admin_input_text"  type="text" size="2" name="answer_price[1]" data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"/>
+                            <input  class="admin_input_text"  type="text" size="2" name="answer_price[0]" value="<?=$answer_price[0]?>" data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"/>
                         </td>
                     </tr>
                     <tr>
 			<td>Ответ 2:</td>
-                        <td><textarea name="text_answer[2]" maxlength="150" rows="2" cols="75" required style="resize: none;"  data-validation="length" data-validation-length="max150" data-validation-error-msg="Допустимая длина не более 150 символов!"></textarea>
+                        <td><textarea name="text_answer[1]" maxlength="150" rows="2" cols="75" required style="resize: none;"  data-validation="length" data-validation-length="max150" data-validation-error-msg="Допустимая длина не более 150 символов!"><?=$text_answer[1]?></textarea>
                             <!--<input class="field_text_edit" id="text_question" name="text_question" value="{$text_question}">-->
                         </td>
                         
@@ -77,7 +107,21 @@
                     <tr>
                         <td>Штраф за ответ:</td>
                         <td>
-                            <input class="admin_input_text" type="text" size="2" name="answer_price[2]" data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"/>
+                            <input class="admin_input_text" type="text" size="2" name="answer_price[1]" value="<?=$answer_price[1]?>" data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"/>
+                        </td>
+                    </tr>
+                    <tr>
+			<td>Ответ 3:</td>
+                        <td>
+                            <textarea name="text_answer[2]" maxlength="150" rows="2" cols="75" required style="resize: none;" data-validation="length" data-validation-length="max150" data-validation-error-msg="Допустимая длина не более 150 символов!"><?=$text_answer[2]?></textarea>
+                            <!--<input class="field_text_edit" id="text_question" name="text_question" value="{$text_question}">-->
+                        </td>
+                        
+                    </tr>
+                    <tr>
+                        <td>Штраф за ответ:</td>
+                        <td>
+                            <input class="admin_input_text" type="text" size="2" name="answer_price[2]" value="<?=$answer_price[2]?>" data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"/>
                         </td>
                     </tr>
                 </table>
@@ -98,39 +142,61 @@
 			break;
                 case 21: //Фото-вопрос
 		?>
-                    
+                    <input type='hidden' name='download_sv' value='<?=$download_sv?>'/>
                     <table>
                         <tr>
                             <td>
                                 Изображение:
                             </td>
                             <td>
+                            <?php
+                            if ($download_sf)
+                            {
+                            ?>
+                                <img src="/storage/photo_questions/<?=$download_sf?>" height="100"/>
+                            <?php
+                            }
+                            ?>
                                 <input type=file id=download_sf name=download_sf accept=image/png,image/jpeg>
                             </td>
                         </tr>
                     <tr>
 			<td>Ответ 1:</td>
                         <td>
-                            <textarea name="text_answer[1]" maxlength="150" rows="2" cols="75" required style="resize: none;"  data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"></textarea>
+                            <textarea name="text_answer[0]" maxlength="150" rows="2" cols="75" required style="resize: none;"  data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"><?=$text_answer[0]?></textarea>
                         </td>
                     </tr>
                     <tr>
                         <td>Штраф за ответ:</td>
                         <td>
-                            <input  class="admin_input_text"  type="text" size="2" name="answer_price[1]" data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"/>
+                            <input  class="admin_input_text"  type="text" size="2" name="answer_price[0]" value="<?=$answer_price[0]?>" data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"/>
                         </td>
                     </tr>
                     <tr>
 			<td>Ответ 2:</td>
                         <td>
-                            <textarea name="text_answer[2]" maxlength="150" rows="2" cols="75" required style="resize: none;"  data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"></textarea>
+                            <textarea name="text_answer[1]" maxlength="150" rows="2" cols="75" required style="resize: none;"  data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"><?=$text_answer[1]?></textarea>
                         </td>
                         
                     </tr>
                     <tr>
                         <td>Штраф за ответ:</td>
                         <td>
-                            <input class="admin_input_text" type="text" size="2" name="answer_price[2]" data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"/>
+                            <input class="admin_input_text" type="text" size="2" name="answer_price[1]" value="<?=$answer_price[1]?>" data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"/>
+                        </td>
+                    </tr>
+                    <tr>
+			<td>Ответ 3:</td>
+                        <td>
+                            <textarea name="text_answer[2]" maxlength="150" rows="2" cols="75" required style="resize: none;" data-validation="length" data-validation-length="max150" data-validation-error-msg="Допустимая длина не более 150 символов!"><?=$text_answer[2]?></textarea>
+                            <!--<input class="field_text_edit" id="text_question" name="text_question" value="{$text_question}">-->
+                        </td>
+                        
+                    </tr>
+                    <tr>
+                        <td>Штраф за ответ:</td>
+                        <td>
+                            <input class="admin_input_text" type="text" size="2" name="answer_price[2]" value="<?=$answer_price[2]?>" data-validation="number" data-validation-allowing="integer,positive" data-validation-error-msg="Введите целое число, большее или равное нулю"/>
                         </td>
                     </tr>
                 </table>
